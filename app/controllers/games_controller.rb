e56fdec152c2126fe5924c39c046338c71a69801
Che To <chekinto@hotmail.com>
2019-05-09 18:50:50 +0100

    Longest word game ever

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+require 'json'
+
+
+class GamesController < ApplicationController
+
+  LETTERS = ('A'..'Z').to_a
+
+  def new
+    @letters = LETTERS.sample(10)
+  end
+
+  def score
+    display = params[:display].split("")
+    attempt = params[:word]
+    result = attempt.upcase.chars.all? { |letter| attempt.upcase.chars.count(letter) <= display.count(letter)}
+    if result
+      puts @sentence = search(attempt)
+    else
+      puts @sentence = "The word can't be built out of the original grid"
+    end
+  end
+end
+
+def search(word)
+  url = "https://wagon-dictionary.herokuapp.com/#{word}"
+  dictionary = open(url).read
+  word_search = JSON.parse(dictionary)
+  if word_search["found"]
+    @sentence = "Congrats! You gained #{word_search["length"]}points!"
+  else
+    @sentence = "Sorry, this is not a word"
+  end
+end
+
